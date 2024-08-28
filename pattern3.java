3c1cd195c94ab087f4d7dc064014eb65707249a0
akshansh496 <guptaakshansh1@gmail.com>
2024-08-28 15:18:47 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class pattern3 {
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no. of rows");
+        int row=sc.nextInt();
+        System.out.println("Enter no. of columns");
+        int column=sc.nextInt();
+        for(int i=1;i<=row;i++)
+        {
+          for(int j=1;j<=i;j++)
+          {
+            System.out.print(j);
+          }System.out.println("");
+          
+        }
+    }
+}
