04b2bf65c43614202c2c9aa6162727f5f5170104
akshansh496 <guptaakshansh1@gmail.com>
2024-09-03 18:51:47 +0530

    Add files via upload

---
+++ pattern_invereted_half_pyramid.java
@@ -0,0 +1,15 @@
+public class pattern_invereted_half_pyramid {
+  
+    public static void main(String[] args){
+      int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if((i+j)>=(n-1))
+               System.out.print("*");
+               else 
+               System.out.print(" ");
+        }System.out.println();
+}
+}
+}
+
