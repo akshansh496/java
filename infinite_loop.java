cbec047d54301b942193de69535b63927af64aeb
akshansh496 <guptaakshansh1@gmail.com>
2024-09-12 11:38:18 +0530

    Add files via upload

---
+++ infinite_loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class infinite_loop {
+    public static void main(){
+        int i=0;
+        do{
+            System.out.println("Hello_World");
+            i++;
+        }while(i>0);
+    }
+}
